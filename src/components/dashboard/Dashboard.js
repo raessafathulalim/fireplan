2e663ea7fd1f1f26792e22d618f4e363930a8517
Raessa Fathul Alim <raessa_pro@mpotech.net>
2019-07-16 03:45:23 +0800

    fix ordered projects

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -39,7 +39,7 @@ const mapStateToProps = (state) => {
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        {collection: 'projects'},
-        {collection: 'notifications', limit: 3}
+        {collection: 'projects', orderBy: ['createdAt', 'desc']},
+        {collection: 'notifications', limit: 5, orderBy: ['time', 'desc']}
     ])
 )(Dashboard)
