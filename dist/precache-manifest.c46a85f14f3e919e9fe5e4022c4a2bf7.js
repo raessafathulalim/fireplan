b88be182a4e2e739c2976d7f5ce904dbb1a69c9e
Raessa Fathul Alim <raessa_pro@mpotech.net>
2019-07-16 03:51:20 +0800

    add building final project

---
+++ dist/precache-manifest.c46a85f14f3e919e9fe5e4022c4a2bf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f284576a6866be6fe5fd33fda8728482",
+    "url": "/index.html"
+  },
+  {
+    "revision": "520864db46fc1bc0166b",
+    "url": "/static/css/main.168f1ce8.chunk.css"
+  },
+  {
+    "revision": "18234c80692462ab952f",
+    "url": "/static/js/2.5ac8bd03.chunk.js"
+  },
+  {
+    "revision": "520864db46fc1bc0166b",
+    "url": "/static/js/main.70d89e6f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
