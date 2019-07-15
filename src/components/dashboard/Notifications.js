d0c52b1c85418766e030f0a169dfdb788ca68e95
Raessa Fathul Alim <raessa_pro@mpotech.net>
2019-07-16 03:42:02 +0800

    add firebase function and log notification

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import moment from 'moment'
 
-const Notifications = () => {
+const Notifications = (props) => {
+
+    const {notifications} = props
+
     return (
-        <div>
-            <div>
-                <p>Notifications</p>
+        <div className="section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Notifications</span>
+                    <ul className="contification">
+                        {notifications && notifications.map(item => {
+                            return (
+                                <li key={item.id}>
+                                    <span className="pink-text">{item.user} </span>
+                                    <span>{item.content}</span>
+                                    <div className="grey-text noted-date">{moment(item.time.toDate()).fromNow()}</div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
             </div>
         </div>
     )
